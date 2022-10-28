fb36699d0da701f4bc95e4862c27c70f54f014ad
Mohammad Rashed Imam <rashedrobin2150@yahoo.com>
2022-10-28 10:08:43 -0400

    a.js

---
+++ a.js
@@ -0,0 +1,100 @@
+/*let x = 0
+while (x <= 5){
+  console.log( x )
+  x = x + 1
+} */
+
+/*let x = 1 ;
+while (x <= 20){
+    if ( x % 3 === 0 && x % 5 === 0) {
+        console.log("juliajames")
+    } else if ( x % 3 === 0 ){ 
+        console.log("julia")
+    } else if ( x % 5 === 0) {
+        console.log("james")
+    } else{
+        console.log(x)
+    }
+    x = x+1
+} */
+
+/*let y = 9
+while (y >= 1){
+    if(y === 1) {console.log(y + ' b ' + y + ' a ' + (y-1))}
+    else if(y === 2){console.log(y + " c " + y + ' d '+ (y-1))}
+    else{console.log(y+' c '+ y + ' hg ' + (y-1))}
+    y = y - 1
+} */
+
+// Let us take an iteration variable, that represent the remaining Time in seconds
+/* n = 60
+
+// While loop with a stop condition
+while (n >= 0) {
+    if (n === 50) {
+        console.log("Orbiter transfers from ground to internal power");
+    }
+    else if (n === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start");
+    }
+    else if (n === 16) {
+        console.log("Activate launch pad sound suppression system");
+    }
+    else if (n === 10) {
+        console.log("Activate main engine hydrogen burnoff system");
+    }
+    else if (n === 6) {
+        console.log("Main engine start");
+    }
+    else if (n === 0) {
+        console.log("Solid rocket booster ignition and liftoff!");
+    }
+    else{
+        console.log("T-"+n+" seconds");
+    }
+
+    //Never forget to decrement/increment the iteration variable in a while loop
+    // Otherwise, you loop will run infinite iterations
+    n = n-1;
+} */
+
+for (let x = 9 ; x >= 1 ; x = x-1) {
+    console.log("hello " + x);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
